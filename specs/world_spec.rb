77f9a0a1ff1c6ada0e82b4bff8a47da4d029b4e1
Valerie Gibson <vgibson14@gmail.com>
2016-07-21 15:16:47 +0100

    adds pokemon to the world

--- specs/world_spec.rb
+++ specs/world_spec.rb
@@ -11,11 +11,15 @@ class WorldTest < Minitest::Test
     @pikachu = Pokemon.new("Pikachu", 25)
     @charmander = Pokemon.new("Charmander", 4)
     @pokemon = [@pikachu, @charmander]
-    @world = World.new(@trainer)
+    @world = World.new(@trainer, @pokemon)
   end
 
   def test_world_has_trainer
     assert_equal(@world.trainer, @trainer)
+  end
+
+  def test_world_has_pokemon
+    assert_equal(@world.pokemon, @pokemon)
   end
 
 end
--- world.rb
+++ world.rb
@@ -1,8 +1,13 @@
 class World
   attr_reader :trainer
 
-  def initialize(trainer)
+  def initialize(trainer, pokemon)
     @trainer = trainer
+    @pokemon = pokemon
+  end
+
+  def pokemon
+    return @pokemon.clone
   end
   
 end
